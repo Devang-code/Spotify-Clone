df9c33e4a04471db59cd3aaa4882d9ea69d1ae60
PRAJAPATI DEVANG <73606846+Devang-code@users.noreply.github.com>
2022-12-23 14:46:27 +0530

    fix

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 console.log("Spotify");
 
-let songIndex = 0;
+let songIndex = 1;
 let audioElement = new Audio("songs/1.mp3");
 let playButton = document.getElementById("playButton");
 let progressBar = document.getElementById("progressBar");
 let songItem = Array.from(document.getElementsByClassName("songItems"));
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+
 
 const songs = [
     { song: "Keshariya", file: "songs/1.mp3", cover: "cover/cover1.jpg" },
@@ -39,12 +42,37 @@ progressBar.addEventListener('change', () => {
 
 songItem.forEach((element) => {
     element.addEventListener('click', (e) => {
-        progressBar.value = 0;
         songIndex = parseInt(e.target.id)
-        console.log(songIndex)
-        audioElement.src = `songs/${songIndex + 1}.mp3`
+        audioElement.currentTime = 0;
+        audioElement.src = `songs/${songIndex}.mp3`
         audioElement.play()
         playButton.classList.remove("fa-circle-play")
         playButton.classList.add("fa-circle-pause")
     })
+})
+
+prev.addEventListener('click', () => {
+    if (songIndex <= 1) {
+        songIndex = 2
+    } else {
+        songIndex -= 1;
+    }
+    audioElement.currentTime = 0;
+    audioElement.src = `songs/${songIndex}.mp3`
+    audioElement.play()
+    playButton.classList.remove("fa-circle-play")
+    playButton.classList.add("fa-circle-pause")
+})
+
+next.addEventListener('click', () => {
+    if (songIndex >= 2) {
+        songIndex = 1
+    } else {
+        songIndex += 1;
+    }
+    audioElement.currentTime = 0;
+    audioElement.src = `songs/${songIndex}.mp3`
+    audioElement.play()
+    playButton.classList.remove("fa-circle-play")
+    playButton.classList.add("fa-circle-pause")
 })
